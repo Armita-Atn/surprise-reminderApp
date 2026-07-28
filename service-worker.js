ba68ad7bdb18337084cf88a40cde9aa39834aa50
Armita-Atn <atlikhanbaajmaloo.armita@gmail.com>
2026-07-28 14:32:32 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yaadaavar-v1';
+const CACHE_NAME = 'yaadaavar-v2';
 const ASSETS = [
   './index.html',
   './app.js',
@@ -20,7 +20,13 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
 
